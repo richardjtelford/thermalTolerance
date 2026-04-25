6c165561952b6c60fcc71e36b27f39ffa95189ac
RichardJTelford <Richard.Telford@uib.no>
2026-04-25 15:10:45 +0200

    capture T50 ci

--- R/summary.htol.R
+++ R/summary.htol.R
@@ -8,14 +8,18 @@
 summary.htol <- function(object, ...) {
   object$results |>
     map(\(x) {
-      T50_ci <- quantile(x$T50, c(0.025, 0.975), na.rm = TRUE)
+      T50_ci <- quantile(x$T50, c(0.025, 0.5, 0.975), na.rm = TRUE) |> 
+        unname()
 
 
       data.frame(
         prop_success = mean(!is.na(x$T50)),
-        Tcrit.mn = mean(na.omit(x$Tcrit)),
-        T50.mn = mean(na.omit(x$T50)),
-        T95.mn = mean(na.omit(x$T95))
+        Tcrit.mn = mean(x$Tcrit, na.rm = TRUE),
+        T50.mn = mean(x$T50, na.rm = TRUE),
+        T50_ci_low = T50_ci[1],
+        T50_ci_median = T50_ci[2],
+        T50_ci_high = T50_ci[3],
+        T95.mn = mean(x$T95, na.rm = TRUE)
       )
     }) |>
     list_rbind(names_to = "id")
